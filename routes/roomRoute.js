cff1ad6f1843c4fc7d6a45cdd91c39fec7ee8dfe
Alexander Perneel <alexander.perneel@student.howest.be>
2025-01-21 14:45:37 +0100

    double socket

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -3,6 +3,72 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 const db = require('../config/db'); 
+const { get } = require('http');
+
+
+router.post('/joinroom'), async (req, res) => {
+  const {roompassword, roomname } = req.body;
+  const checkRoomNameQuery = 'SELECT * FROM gamerooms WHERE name = ? and password = ?';
+  const roomDetails = await db.query(checkRoomNameQuery, [roomname, roompassword]);
+  if(roomDetails.length === 0) {
+    return res.json({
+      type: 'error',
+      message: 'Deze kamer bestaat niet'
+    });
+  }
+  else
+  {
+    const joinRoomQuery = 'INSERT INTO gameroom (userId, roomId) VALUES (?, ?)';
+    await db.query(joinRoomQuery, [req.session.userId, roomDetails[0].roomId]);
+    res.redirect('/room/' + roomname);
+  }
+
+
+}
+
+router.post('/createroom', async (req, res) => {
+  console.log("dit is de aller eerste test");
+  
+  let { roompassword, roomname } = req.body;
+  console.log(req.body);
+
+  // Check if the room name already exists
+  const checkRoomNameQuery = 'SELECT * FROM gamerooms WHERE name = ?';
+  const roomDetails = await db.query(checkRoomNameQuery, [roomname]);
+
+  if (roomDetails.length !== 0) {
+    return res.json({
+      type: 'error',
+      message: 'Deze kamernaam bestaat al'
+    });
+  }
+
+  console.log("dit is test2");
+  
+
+  
+  //check if password is empty
+  if(roompassword === '') {
+    roompassword = null;
+  }
+  const createRoomQuery = 'INSERT INTO gamerooms (name, password) VALUES (?, ?)';
+  await db.query(createRoomQuery, [roomname, roompassword]);
+  //get the id of last inserted room
+  const getRoomIdQuery = 'Select * from gamerooms where name = ?';
+  const getId = await db.query(getRoomIdQuery, [roomname]);
+
+  const insertPlayerInRoomQuery = 'INSERT INTO gameroom (userId, roomId) VALUES (?, ?)';
+  await db.query(insertPlayerInRoomQuery, [req.session.userId, getId[0].roomId]);
+
+  // Respond with success
+  res.json({
+    type: 'succes',
+    message: roomname
+  });
+});
+
+
+
 
 
 router.get('/room/:roomId', async (req, res) => {
